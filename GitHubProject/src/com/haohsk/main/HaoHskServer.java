2353aa3da325e224e8714595fd3a57ea6bb26433
David.Yoon <tun7korea@gmail.com>
2015-05-08 20:41:15 +0900

    오늘 한거야.ㅋㅋㅋ

--- GitHubProject/src/com/haohsk/main/HaoHskServer.java
+++ GitHubProject/src/com/haohsk/main/HaoHskServer.java
@@ -1,53 +1,75 @@
 package com.haohsk.main;
 
-import org.vertx.java.core.Handler;
-import org.vertx.java.core.http.HttpServer;
-import org.vertx.java.core.http.HttpServerRequest;
-import org.vertx.java.core.logging.Logger;
-import org.vertx.java.core.net.NetSocket;
-import org.vertx.java.core.streams.Pump;
-import org.vertx.java.platform.Verticle;
+import io.vertx.core.MultiMap;
+import io.vertx.core.Vertx;
+import io.vertx.core.http.HttpServer;
 
-public class HaoHskServer extends Verticle {
+public class HaoHskServer {
 
+	public static void main(String[]args){
+		HaoHskServer hhs = new HaoHskServer();
+		hhs.start();
+	}
+	
 	public void start() {
-		HttpServer server = vertx.createHttpServer();
+		Vertx vertx = Vertx.vertx();
 
-		server.requestHandler(new Handler<HttpServerRequest>() {
-			public void handle(HttpServerRequest req) {
-				String file = "";
-				if (req.path().equals("/")) {
-					file = "index.html";
-				}else if(req.path().equals("/signpage")){
-					System.out.println("start - signpage html");
-					file = "signpage.html";
-				}else if(req.path().equals("/signupdate")){
-					System.out.println("start - signupdate");
-					file = "signupdate.html";
-				}else if(req.path().equals("/signprofile")){
-					System.out.println("start -  signprofile");
-					file = "signprofile.html";
-				}else if(req.path().equals("/boardcreate")){
-					System.out.println("start - boardcreate ");
-					file = "boardcreate.html";
-				}else if(req.path().equals("/boardread")){
-					System.out.println("start -  boardread");
-					file = "boardread.html";
-				}else if(req.path().equals("/boardupdate")){
-					System.out.println("start - boardupdate ");
-					file = "boardupdate.html";
-				}else if(req.path().equals("/boarddelete")){
-					System.out.println("start - boarddelete ");
-					file = "boarddelete.html";
-				}else if(req.path().equals("/loginpage")){
-					System.out.println("start - loginpage ");
-					file = "loginpage.html";
-				}else if (!req.path().contains("..")) {
-					System.out.println(" ..");
-					file = req.path();
-				}
-				req.response().sendFile("web/" + file);
+		HttpServer httpServer = vertx.createHttpServer();
+
+		httpServer.requestHandler(req ->{
+			System.out.println("vertx port = 9090");
+			String filename = null;
+			String dir = System.getProperty("user.dir");
+			if (req.path().equals("/")) {
+				filename = "/index.html";
+			} else if (req.path().equals("/index")) {
+				filename = "/index.html";
+			}else if (req.path().equals("/boardcreate")) {
+				
+				filename = "/boardcreate.html";
+			} else if (req.path().equals("/boarddelete")) {
+				filename = "/boarddelete.html";
+			}else if (req.path().equals("/boardread")) {
+				filename = "/boardread.html";
+			}else if (req.path().equals("/boardupdate")) {
+				filename = "/boardupdate.html";
+			}else if (req.path().equals("/formpage")) {
+				filename = "/formpage.html";
+			}else if (req.path().equals("/indexSiteMap")) {
+				filename = "/indexSiteMap.html";
+			} else if (req.path().equals("/loginpage")) {
+				filename = "/loginpage.html";
+			}else if (req.path().equals("/signpage")) {
+				filename = "/signpage.html";
+			}else if (req.path().equals("/loginpage")) {
+				filename = "/loginpage.html";
+			}else if (req.path().equals("/signprofile")) {
+				filename = "/signprofile.html";
+			}else if (req.path().equals("/signupdate")) {
+				filename = "/signupdate.html";
+			} else if (req.uri().startsWith("/form")) {
+				req.response().setChunked(true);
+				req.setExpectMultipart(true);
+
+				req.endHandler((v) -> {
+					MultiMap mm = req.formAttributes();
+					for (String attr : mm.names()) {
+						req.response().write("Got attr " + attr + " : " + req.formAttributes().get(attr) + "\n");
+					}
+					req.response().end();
+				});
+			} 
+
+			else {
+				req.response().setStatusCode(404).end();
 			}
-		}).listen(8080, "localhost");
+			if (filename != null) {
+				System.out.println(dir + "/src/com/haohsk/main/web" + filename);
+				req.response().sendFile(dir+"/src/com/haohsk/main/web"+filename);
+			}
+
+		});
+
+		httpServer.listen(9090);
 	}
 }
